bceb4a9f8c9af41ea0c90ef257d4922e631c3583
Faruks23 <mdfaruksp2@gmail.com>
2024-09-14 17:41:52 +0600

    update product section

--- src/app/shared/ProductCard/ProductCard.tsx
+++ src/app/shared/ProductCard/ProductCard.tsx
@@ -5,15 +5,16 @@ export default function ProductCard({ item }:any) {
   return (
     <div className='relative h-[445px] w-full max-w-[285px] border group'>
       {/* Product Image */}
-      <Image src={item.image} width={285} height={300} alt='product-image' />
+      <Image src={item.image} width={285} height={300} alt='product-image' className=' h-[300px]' />
 
       {/* Product Body */}
-      <div className="product-body p-4 space-y-2">
+      <div className="product-body p-4  scroll-py-1 md:space-y-2">
         <h1 className='text-[24px] font-semibold'>{item.name}</h1>
         <p className='text-[#898989] font-medium'>{item.subName}</p>
         <p className='text-[20px] font-semibold'>
-          Rp {item.price}
-          <span className='ml-2 text-[#B0B0B0] font-normal text-base'>Rp {item.mainPrice}</span>
+          Rp {item.price} 
+          <br className=' block md:hidden' />
+          {item.mainPrice && <span className='ml-2 text-[#B0B0B0] font-normal text-base'>Rp {item?.mainPrice}</span>}
         </p>
       </div>
 
@@ -30,7 +31,23 @@ export default function ProductCard({ item }:any) {
           <FaExchangeAlt className="hover:text-gray-300 cursor-pointer" />
           <FaHeart className="hover:text-gray-300 cursor-pointer" />
         </div>
+
+        
+
       </div>
+      {item.status === 'New' && <>
+        {/* badge  */}
+        <div className={` absolute top-5 right-4 badge size-12  rounded-full bg-[#2EC1AC]  flex justify-center items-center px-3 py-3`}>
+          <h1 className='text-white text-base font-medium'>{item?.status}</h1>
+        </div>
+      </>}
+      {item.discount && <>
+        {/* badge  */}
+        <div className={` absolute top-5 right-4 badge size-12  rounded-full bg-[#E97171]  flex justify-center items-center px-3 py-3`}>
+          <h1 className='text-white text-base font-medium'>{item?.discount}</h1>
+        </div>
+      </>}
+
     </div>
   );
 }
